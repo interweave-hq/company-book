f7390ee372f22e7c50061ff86c0a66232ad3da24
Mike Carbone <mfcbone2@gmail.com>
2023-10-09 14:50:42 -0400

    :rocket: Adds launch page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 const withNextra = require("nextra")({
-	theme: "nextra-theme-docs",
-	themeConfig: "./theme.config.jsx",
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        port: "",
+        pathname: "/carbonemike-images/**"
+      }
+    ]
+  }
 });
 
 module.exports = withNextra();
